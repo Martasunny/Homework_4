228755dac4f50b81d963f38e1a15209573f4e39c
Marta <[martasunnyy@gmail.com]>
2022-09-05 20:04:26 +0300

    решена задача 27

--- задача_27/Program.cs
+++ задача_27/Program.cs
@@ -3,4 +3,16 @@
 Console.WriteLine("Enter number");
 int NumberA = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(NumberA);
+int result = 0;
+
+for (int i = 0; i < stringNumber.Length; i++) 
+{
+    result = result + Convert.ToInt32(stringNumber[i].ToString());
+
+}
+
+Console.WriteLine(result);
+
+
+
 
